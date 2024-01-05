134a3d79086da0a6471ac08879e9a50ccb1de5de
Kamsi-yonna <etiobikamsi@gmail.com>
2024-01-05 02:29:50 +0100

    feat - index page featured article section

---
+++ components/App/ArticleCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <NuxtLink
+    class="group flex items-center justify-between"
+    target="_blank"
+    :to="article.url">
+    <article>
+      <div class="relative z-10 order-first mb-3 flex items-center text-sm text-gray-400 dark:text-gray-500 pl-3.5">
+        <span
+          aria-hidden="true"
+          class="absolute inset-y-0 left-0 flex items-center">
+          <span class="h-4 w-0.5 rounded-full bg-gray-200 dark:bg-gray-500"></span>
+        </span>
+        {{ article.datePublished }}
+      </div>
+      <h2
+        class="text-base font-semibold font-display tracking-tight text-gray-800 dark:text-gray-100 group-hover:text-primary-600">
+        {{ article.name }}
+      </h2>
+      <p class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400">
+        {{ article.description }}
+      </p>
+    </article>
+    <NuxtImg
+      alt="Kamsiyonna Etiobi"
+      class="hover:ring-4 transition-all duration-300 rounded-sm h-12 w-12 sm:h-12 sm:w-12 object-cover"
+      format="webp"
+      placeholder
+      sizes="48px sm:64px"
+      src="/images/Notion_app_logo.png" />
+  </NuxtLink>
+</template>
+
+<script setup>
+defineProps({
+  article: {
+    type: Object,
+    required: true
+  }
+})
+</script>
--- components/Home/Articles.vue
+++ components/Home/Articles.vue
@@ -1,31 +1,11 @@
 <template>
   <div>
     <h2 class="uppercase text-xs font-semibold text-gray-400 mb-6">RECENT ARTICLES</h2>
-    <ul class="space-y-16">
-      <li>
-        <!-- APP ARTICLE CARD START  -->
-
-        <NuxtLink class="group">
-          <article>
-            <time
-              class="relative z-10 order-first mb-3 flex items-center text-sm text-gray-400 dark:text-gray-500 pl-3.5"
-              datetime="2022-09-05">
-              <span
-                aria-hidden="true"
-                class="absolute inset-y-0 left-0 flex items-center">
-                <span class="h-4 w-0.5 rounded-full bg-gray-200 dark:bg-gray-500"></span>
-              </span>
-              getReadableDate article.published
-            </time>
-            <h2
-              class="text-base font-semibold font-display tracking-tight text-gray-800 dark:text-gray-100 group-hover:text-primary-600">
-              article.title
-            </h2>
-            <p class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400">article.description</p>
-          </article>
-        </NuxtLink>
-
-        <!-- APP ARTICLE CARD TWO  -->
+    <ul class="space-y-8">
+      <li
+        v-for="(article, id) in articles"
+        :key="id">
+        <ArticleCard :article="article" />
       </li>
     </ul>
     <div class="flex items-center justify-center mt-6 text-sm">
@@ -38,4 +18,19 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+const articles = [
+  {
+    name: 'Personal Goals Roadmap',
+    description: 'A career and personal growth guide',
+    datePublished: 'Jan 5, 2024',
+    url: 'https://www.notion.so/Personal-Goals-Roadmap-9f03de828e454e42ab940ce9e8a38ef6'
+  },
+  {
+    name: 'Strategy planner for 2024',
+    description: 'Strategic Reflection and Planning Guide for Future Success',
+    datePublished: 'Jan 5, 2024',
+    url: 'https://www.notion.so/2024-Strategy-87e024114414406fb4c58ac7d39d920a'
+  }
+]
+</script>
