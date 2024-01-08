dd118e10c6b4eba418ca9a0bb3e7c1845711507e
Kamsi-yonna <etiobikamsi@gmail.com>
2024-01-08 17:16:54 +0100

    fix - projectscard fixes

--- components/App/ProjectCard.vue
+++ components/App/ProjectCard.vue
@@ -1,35 +1,27 @@
 <template>
   <NuxtLink
-    class="flex items-end gap-4 group p-2 -m-2 rounded-lg"
+    class="flex items-end gap-4 group p-2 -m-2 mt-8 rounded-lg"
     external
     target="_blank"
     :to="project.url">
     <div class="max-w-sm">
-      <h3 class="text-sm">{{ props.project.name }}</h3>
-      <p class="text-gray-400 text-sm">{{ props.project.description }}</p>
+      <h3 class="text-sm">{{ project.name }}</h3>
+      <p class="text-gray-400 text-sm">{{ project.description }}</p>
     </div>
 
     <div class="flex-1 border-b border-dashed border-gray-300 dark:border-gray-800 group-hover:border-gray-700"></div>
     <UAvatar
-      :alt="props.project.name"
+      :alt="project.name"
       size="md"
-      :src="props.project.thumbnail"
+      :src="project.thumbnail"
       :ui="{ rounded: 'rounded z-10 relative' }" />
   </NuxtLink>
 </template>
 
 <script setup lang="ts">
-interface ParsedContent {
-  name: string
-  url: string
-  description: string
-  thumbnail: string
-  status?: string // Add any optional properties as needed
-}
-
-const props = defineProps({
+defineProps({
   project: {
-    type: Object as () => ParsedContent,
+    type: Object,
     default: () => ({})
   }
 })
