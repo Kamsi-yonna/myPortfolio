2a135a7f439cfcb65eec60c0573c94d1648a061f
Kamsi-yonna <etiobikamsi@gmail.com>
2024-03-17 11:28:14 +0100

    update - tailwind update on the social links

--- components/Home/Socials.vue
+++ components/Home/Socials.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <h2 class="uppercase text-xs font-semibold text-gray-900 dark:text-gray-400 mb-4 sm:mb-2">FIND ME ON</h2>
-    <div class="space-y-1 sm:flex grid grid-cols-3 gap-1 text-gray-900 dark:text-gray-400">
+    <div class="space-y-1 sm:flex grid grid-cols-1 gap-3 text-gray-900 dark:text-gray-400">
       <NuxtLink
         v-for="link in links"
         :key="link.icon"
-        class="flex items-end gap-2 hover:text-gray-300 dark:hover:text-gray-100"
+        class="flex items-end gap-2 p-4 border border-gray-800 rounded-xl hover:border-primary"
         external
         target="_blank"
         :to="link.url">
