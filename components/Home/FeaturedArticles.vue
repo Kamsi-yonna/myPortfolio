37e239c08349789acb73f103297d99b95f5d1eb3
Kamsi-yonna <etiobikamsi@gmail.com>
2024-04-16 16:18:58 +0100

    feat - article component on index page

--- components/Home/FeaturedArticles.vue
+++ components/Home/FeaturedArticles.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h2 class="uppercase text-xs font-semibold text-gray-400 mb-6">RECENT ARTICLES</h2>
-    <!-- <ArticleCard :articlesToShow="3" /> -->
+    <HomeArticles :articlesToShow="3" />
 
     <div class="flex items-center justify-center mt-6 text-sm">
-      <!-- <UButton
+      <UButton
         color="gray"
         label="All Articles &rarr;"
         to="/articles"
-        variant="link" /> -->
+        variant="link" />
     </div>
   </div>
 </template>
